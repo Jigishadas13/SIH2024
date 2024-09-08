4897814dfb092aed250ce447bf6f0bf0f27aa62b
Jigishadas13 <jigishadas13@gmail.com>
2024-09-08 12:14:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./styles.css";
 import Pie from './components/Pie';
 import ColChart from "./components/ColChart";
-import IndiaMap from "./IndiaMap";
+//import IndiaMap from "./IndiaMap";
+//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//import Cards from './components/Cards';  // Adjusted path to Cards.jsx
+//import AfforestationAnalytics from './components/AfforestationAnalytics';  // Adjusted path to AfforestationAnalytics.jsx
+
 
 
 export default function App() {
@@ -10,6 +14,14 @@ export default function App() {
    
      
      <div><IndiaMap/></div>
+    /* <Router>
+      <Routes>
+        <Route path="/" element={<Cards />} />  
+        <Route path="/afforestation" element={<AfforestationAnalytics />} />  
+      </Routes>
+    </Router>
+  );
+}*/
     
     
   );
